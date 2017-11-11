2baaee2a41e23247942d1f22a34f486d5fe2da9c
wwakas <mipek77@gmail.com>
2017-11-11 15:08:22 +0100

    remake
    usunięcie wpisu dodającego silnik InnoDB

--- baza_hurtownia.sql
+++ baza_hurtownia.sql
@@ -11,7 +11,7 @@ kod_pocztowy int(10) not null,
 telefon int(20) not null,
 nip int(30),
 CONSTRAINT k_pk PRIMARY KEY(id_klienta)
-) ENGINE = InnoDB;
+) ;
 
 CREATE TABLE pracownicy (
 id_pracownika int not null,
@@ -19,21 +19,21 @@ imie varchar(20) not null,
 nazwisko varchar(20) not null,
 adres varchar(30),
 CONSTRAINT p_pk PRIMARY KEY(id_pracownika)
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE magazyn (
 id_produktu int not null,
 cena float,
 ilosc int,
 CONSTRAINT m_pk PRIMARY KEY(id_produktu)
-)ENGINE=InnoDB;
+);
 
 CREATE TABLE dostawcy(
 id_dostawcy int not null,
 nazwa_dostawcy varchar(30),
 email varchar(20),
 CONSTRAINT d_pk PRIMARY KEY(id_dostawcy)
-) ENGINE=InnoDB;
+);
 
 CREATE TABLE logowanie_klienta(
 id_klienta int not null,
@@ -41,7 +41,7 @@ login_klienta varchar(20) not null,
 haslo_klienta varchar(20) not null,
 CONSTRAINT log_pk PRIMARY KEY(id_klienta),
 CONSTRAINT log_fk FOREIGN KEY(id_klienta) REFERENCES  klienci(id_klienta)
-) ENGINE=InnoDB;
+);
 
 CREATE TABLE logowanie_pracownika(
 id_pracownika int not null,
@@ -49,7 +49,7 @@ login_pracownika varchar(20) not null,
 haslo_pracownika varchar(20) not null,
 CONSTRAINT logp_pk PRIMARY KEY(id_pracownika),
 CONSTRAINT logp_fk FOREIGN KEY(id_pracownika) REFERENCES pracownicy(id_pracownika)
-) ENGINE=InnoDB;
+);
 
 CREATE TABLE baza_produktow(
 id_produktu int not null,
@@ -60,7 +60,7 @@ id_dostawcy int not null,
 CONSTRAINT bp_pk PRIMARY KEY(id_produktu),
 CONSTRAINT bp_fk1 FOREIGN KEY(id_produktu) REFERENCES magazyn(id_produktu),
 CONSTRAINT bp_fk2 FOREIGN KEY(id_dostawcy) REFERENCES dostawcy(id_dostawcy)
-) ENGINE=InnoDB;
+);
 
 CREATE TABLE zamowienie(
 id_zamowienia int not null,
@@ -70,7 +70,7 @@ status ENUM('zamowione', 'realizacja', 'wyslane'),
 data_doreczenia DATE,
 CONSTRAINT zm_pk PRIMARY KEY(id_zamowienia),
 CONSTRAINT zm_fk1 FOREIGN KEY(id_klienta) REFERENCES logowanie_klienta(id_klienta)
-) ENGINE=InnoDB;
+);
 
 CREATE TABLE dane_zamowienia(
 id_produktu int not null,
@@ -78,7 +78,7 @@ id_zamowienia int not null,
 CONSTRAINT dz_pk PRIMARY KEY(id_produktu, id_zamowienia),
 CONSTRAINT dz_fk FOREIGN KEY(id_produktu) REFERENCES baza_produktow(id_produktu),
 CONSTRAINT dz_fk1 FOREIGN KEY(id_zamowienia) REFERENCES zamowienie(id_zamowienia)
-)ENGINE=InnoDB;
+);
 
 
 

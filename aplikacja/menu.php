af40730724e26f5ab901e76f5ce6c731469a84a2
wwakas <mipek77@gmail.com>
2017-11-28 13:53:56 +0100

    menu front part1
    up

--- aplikacja/menu.php
+++ aplikacja/menu.php
@@ -2,27 +2,35 @@
 echo"<div id='NAGLOWEK'><img src='images/logo.png' class=srodek></div>
 	<a href=wyloguj.php>Wyloguj</a>
 	<div id='MENU'>
-		  <ul>
-			  <li><a href=''>Realizuj zamówienie</a></li>
-			  <li><a href='zmien_stan_zamowienia.php'>Zmień stan zamówienia</a></li>
-			  <li><a href=''>Zamów towar do magazynu</a></li>
-			  <li><a href='sprawdz_stan_magazynu.php'>Sprawdź stan magazynu</a></li>
-			  <li><a href=''>Generuj fakturę</a></li>
-		</ul></div>
-			<div id='MENU'>
-		  <ul>
-			  <li><a href=''>Wyślij fakturę na email</a></li>
-			  <li><a href=''>Generuj zestawienie</a></li>
-			  <li><a href=''>Złóż zamówienie</a></li>
-			  <li><a href=''>Podgląd zamówień</a></li>
-			  <li><a href='pokaz_koszyk.php'>Pokaż Koszyk</a></li>
-		</ul></div>
-			<div id='MENU'>
-		  <ul>
-			  <li><a href=''>Pobierz fakturę</a></li>
-			  <li><a href='wyswietl_produkty.php'>Podgląd dostępnych produktów</a></li>
-			  <li><a href=''>Wyszukaj produkt</a></li>
-			  <li><a href='sprawdz_stan_zamowienia.php'>Sprawdź stan realizacji zamówienia</a></li>
-			  <li><a href=''>Edytuj dane osobowe</a></li>
-		</ul></div>";
+		  <ol>
+			  <li><a href=''>MAGAZYN</a>
+			  <ul>
+						<li><a href='#'>Zamów towar</a></li>
+						<li><a href='#'>Sprawdź stan</a></li>
+						<li><a href='zmien_stan_zamowienia.php'>Realizuj zamówienie</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>FAKTURY/ZESTAWIENIA</a>
+			  <ul>
+						<li><a href='#'>Generuj fakturę</a></li>
+						<li><a href='#'>Wyślij  fakturę na email</a></li>
+						<li><a href='#'>Generuj zestawienie</a></li>
+						<li><a href='#'>Pobierz fakturę</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>ZAMÓWIENIA</a>
+			  <ul>
+						<li><a href='wyswietl_produkty.php'>Złóż zamówienie</a></li>
+						<li><a href='#'>Podgląd zamówień</a></li>
+						<li><a href='pokaz_koszyk.php'>Pokaż koszyk</a></li>
+						<li><a href='sprawdz_stan_zamowienia.php'>Sprawdź stan realizacji</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>INNE</a>
+			  <ul>
+						<li><a href='#'>Wyszukaj produkt</a></li>
+						<li><a href='#'>Edytuj dane osobowe</a></li>
+					</ul>
+			  </li>
+		</ol></div></br>";
 ?>
---
+++ aplikacja/old/menu_old/menu.php
@@ -0,0 +1,28 @@
+<?php
+echo"<div id='NAGLOWEK'><img src='images/logo.png' class=srodek></div>
+	<a href=wyloguj.php>Wyloguj</a>
+	<div id='MENU'>
+		  <ul>
+			  <li><a href=''>Realizuj zamówienie</a></li>
+			  <li><a href='zmien_stan_zamowienia.php'>Zmień stan zamówienia</a></li>
+			  <li><a href=''>Zamów towar do magazynu</a></li>
+			  <li><a href='sprawdz_stan_magazynu.php'>Sprawdź stan magazynu</a></li>
+			  <li><a href=''>Generuj fakturę</a></li>
+		</ul></div>
+			<div id='MENU'>
+		  <ul>
+			  <li><a href=''>Wyślij fakturę na email</a></li>
+			  <li><a href=''>Generuj zestawienie</a></li>
+			  <li><a href=''>Złóż zamówienie</a></li>
+			  <li><a href=''>Podgląd zamówień</a></li>
+			  <li><a href='pokaz_koszyk.php'>Pokaż Koszyk</a></li>
+		</ul></div>
+			<div id='MENU'>
+		  <ul>
+			  <li><a href=''>Pobierz fakturę</a></li>
+			  <li><a href='wyswietl_produkty.php'>Podgląd dostępnych produktów</a></li>
+			  <li><a href=''>Wyszukaj produkt</a></li>
+			  <li><a href='sprawdz_stan_zamowienia.php'>Sprawdź stan realizacji zamówienia</a></li>
+			  <li><a href=''>Edytuj dane osobowe</a></li>
+		</ul></div>";
+?>

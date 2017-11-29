57a702b7316623d22adc176c0466d788913c3db5
wwakas <mipek77@gmail.com>
2017-11-29 17:32:29 +0100

    podział menu-pracownik/klient
    bump

---
+++ aplikacja/menu_divided/menu_k.php
@@ -0,0 +1,29 @@
+<?php
+echo"<a href=main.php><div id='NAGLOWEK'><img src='images/logo.png' class=srodek></div></a>
+	<a href=wyloguj.php><div id='wyloguj'>Wyloguj</div></a>
+	<div id='MENU'>
+		  <ol>
+			  <li><a href=''>ZAMÓWIENIA</a>
+			  <ul>
+						<li><a href='wyswietl_produkty.php'>Złóż zamówienie</a></li>
+						<li><a href='#'>Podgląd zamówień</a></li>
+						<li><a href='pokaz_koszyk.php'>Pokaż koszyk</a></li>
+						<li><a href='sprawdz_stan_zamowienia.php'>Sprawdź stan realizacji</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>FAKTURY/ZESTAWIENIA</a>
+			  <ul>
+						<li><a href='#'>Generuj fakturę</a></li>
+						<li><a href='#'>Wyślij  fakturę na email</a></li>
+						<li><a href='#'>Generuj zestawienie</a></li>
+						<li><a href='#'>Pobierz fakturę</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>INNE</a>
+			  <ul>
+						<li><a href='#'>Wyszukaj produkt</a></li>
+						<li><a href='#'>Edytuj dane osobowe</a></li>
+					</ul>
+			  </li>
+		</ol></div></br>";
+?>
---
+++ aplikacja/menu_divided/menu_p.php
@@ -0,0 +1,28 @@
+<?php
+echo"<a href=main.php><div id='NAGLOWEK'><img src='images/logo.png' class=srodek></div></a>
+	<a href=wyloguj.php><div id='wyloguj'>Wyloguj</div></a>
+	<div id='MENU'>
+		  <ol>
+			  <li><a href=''>MAGAZYN</a>
+			  <ul>
+						<li><a href='#'>Zamów towar</a></li>
+						<li><a href='sprawdz_stan_magazynu.php'>Sprawdź stan</a></li>
+						<li><a href='zmien_stan_zamowienia.php'>Realizuj zamówienie</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>FAKTURY/ZESTAWIENIA</a>
+			  <ul>
+						<li><a href='#'>Generuj fakturę</a></li>
+						<li><a href='#'>Wyślij  fakturę na email</a></li>
+						<li><a href='#'>Generuj zestawienie</a></li>
+						<li><a href='#'>Pobierz fakturę</a></li>
+					</ul>
+			  </li>
+			  <li><a href=''>INNE</a>
+			  <ul>
+						<li><a href='#'>Wyszukaj produkt</a></li>
+						<li><a href='#'>Edytuj dane osobowe</a></li>
+					</ul>
+			  </li>
+		</ol></div></br>";
+?>
